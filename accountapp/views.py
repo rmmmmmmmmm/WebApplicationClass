792c0825514ef3f703c915ac70d344b200495756
82107 <rmmmm.9@gmail.com>
2021-07-12 11:27:22 +0900

    2021.07.12. WebApplicationClass Commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from accountapp.models import HelloWorld
 
 # Create your views here.
-
 
 def hello_world(request):
     if request.method == "POST":
 
         temp = request.POST.get('input_text')
 
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accoutapp/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accoutapp/hello_world.html',
                       context={'text': 'GET METHOD!'})
