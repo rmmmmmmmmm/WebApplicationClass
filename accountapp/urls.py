64a882566c17b318129de49a07611c4657b6304e
82107 <rmmmm.9@gmail.com>
2021-07-12 12:56:46 +0900

    2021.07.12. WebApplicationClass Commit 4

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -4,6 +4,6 @@ from accountapp.views import hello_world
 
 app_name = 'accountapp' # 라우팅을 편하게 만들어주는 앱네임
 
-urlpatterns = {
+urlpatterns = [
     path('hello_world/', hello_world, name="hello_world"),
-}
+]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
+
 
 # Create your views here.
 
@@ -13,12 +16,9 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        return render(request, 'accoutapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
     else:
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accoutapp/hello_world.html',
+        return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
-
