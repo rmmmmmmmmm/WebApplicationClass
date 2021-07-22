01e2899aa4c7eb8f35378823b1f666928c0b9476
82107 <rmmmm.9@gmail.com>
2021-07-22 12:27:22 +0900

    2021.07.22. WebApplicationClass Commit 3

--- WebApplicationClass/settings.py
+++ WebApplicationClass/settings.py
@@ -136,10 +136,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# head.html에 가져온 소스를 한꺼번에 모아뒀다가 서버에서 요청시 제공
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
